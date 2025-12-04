3b766149217778518ed795e05d4867ec07731234
bspoloo <animetx098@gmail.com>
2025-12-04 15:49:40 -0400

    añadido the forms C#

---
+++ Medico-Forms/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Medico_Forms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonValidar = new System.Windows.Forms.Button();
+            this.textBoxCI = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonValidar
+            // 
+            this.buttonValidar.Location = new System.Drawing.Point(183, 33);
+            this.buttonValidar.Name = "buttonValidar";
+            this.buttonValidar.Size = new System.Drawing.Size(75, 23);
+            this.buttonValidar.TabIndex = 0;
+            this.buttonValidar.Text = "Validar";
+            this.buttonValidar.UseVisualStyleBackColor = true;
+            this.buttonValidar.Click += new System.EventHandler(this.buttonValidar_Click);
+            // 
+            // textBoxCI
+            // 
+            this.textBoxCI.Location = new System.Drawing.Point(63, 35);
+            this.textBoxCI.Name = "textBoxCI";
+            this.textBoxCI.Size = new System.Drawing.Size(100, 20);
+            this.textBoxCI.TabIndex = 1;
+            this.textBoxCI.Text = "6543210";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxCI);
+            this.Controls.Add(this.buttonValidar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonValidar;
+        private System.Windows.Forms.TextBox textBoxCI;
+    }
+}
+
---
+++ Medico-Forms/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Medico_Forms
+{
+    public partial class Form1 : Form
+    {
+        MedicosService.SegipServiceSoapClient segipService = new MedicosService.SegipServiceSoapClient();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonValidar_Click(object sender, EventArgs e)
+        {
+            String CI = textBoxCI.Text;
+            bool isValid = segipService.VerificarCI(CI);
+            if (isValid)
+            {
+                MessageBox.Show("Cédula válida.");
+            }
+            else
+            {
+                MessageBox.Show("Cédula inválida.");
+            }
+        }
+    }
+}
